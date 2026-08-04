09e19bca09682e03a42f46982c9eb24a049fc1a6
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-04 15:26:29 +0530

    RoboShop Project

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,7 +3,7 @@
 MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
 
 echo "First Movie: ${MOVIES[0]}"
-echo "Second Movie: ${MOVIES[3]}"
-echo "Third Movie: ${MOVIES[4]}"
+echo "Second Movie: ${MOVIES[1]}"
+echo "Third Movie: ${MOVIES[2]}"
 
 echo "All movies: ${MOVIES[@]}"
