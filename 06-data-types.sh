7c448ac5b12bee813aefed53b3c205459a42a14d
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-04 15:23:14 +0530

    RoboShop Project

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+NUMBER1=100
+NUMBER2=200
 
-echo "Script ececuted at: $TIMESTAMP"
+TIMESTAMP=$(date)
+ echo "Script ececuted at: $TIMESTAMP"
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
+
+
