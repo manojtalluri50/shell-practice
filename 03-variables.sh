663437587c8a318017adf20ed1cf142fe0f6fe79
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-04 14:21:05 +0530

    RoboShop Project

--- 03-variables.sh
+++ 03-variables.sh
@@ -8,3 +8,8 @@ echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
 echo "$PERSON1: I am fine too. What's up?"
 echo "$PERSON2:: Nohting, just going to Mars now, will you come?"
 echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
+
+
+PERSON3=Modi
+
+echo "$PERSON3"
