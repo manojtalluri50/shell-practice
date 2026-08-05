391738f8fcfe862e796ea43228f6381ff6ec1e5a
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-05 12:20:17 +0530

    RoboShop Project

---
+++ 13-install-command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+    echo "You are running with the root access"
+fi
