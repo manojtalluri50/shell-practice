e07415104991aa76f547cce2a2d9ac7fc5dd56d0
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-05 12:44:35 +0530

    RoboShop Project

--- 13-install-command.sh
+++ 13-install-command.sh
@@ -15,16 +15,24 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then 
     echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing MySQL is ... SUCCESS"
+    else
+        echo "Installing MySQL is ... FAILURE"
+        exit 1
+    fi
 else
     echo "MySQL is already installed... Nothing to do"
 fi
 
-dnf install mysql -y
+# dnf install mysql -y
 
-if [ $? -eq 0 ]
-then
-    echo "Installing MySQL is ... SUCCESS"
-else
-    echo "Installing MySQL is ... FAILURE"
-    exit 1
-fi
+# if [ $? -eq 0 ]
+# then
+#     echo "Installing MySQL is ... SUCCESS"
+# else
+#     echo "Installing MySQL is ... FAILURE"
+#     exit 1
+# fi
