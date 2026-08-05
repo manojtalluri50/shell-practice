6a27c18c17843922773be814e061c3fdba674595
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-05 13:31:00 +0530

    RoboShop Project

---
+++ 14-functions.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+    echo "You are running with the root access"
+fi
+
+
+# validate function takes inputs exit status as $1 and which command they try to install as $2
+VALIDATE(){
+        if [ $1 -eq 0 ]
+    then
+        echo "Installing $2 is ... SUCCESS"
+    else
+        echo "Installing $2 is ... FAILURE"
+        exit 1
+    fi
+
+}
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo "MySQL is already installed... Nothing to do"
+fi
+
+dnf list installed python3
+
+if [ $? -ne 0 ]
+then 
+    echo "Python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "Python3"
+else
+    echo "Python3 is already installed... Nothing to do"
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+    echo "Nginx is not installed... going to install it"
+    dnf install nginx -y
+     VALIDATE $? "Nginx"
+else
+    echo "Nginx is already installed... Nothing to do"
+fi
