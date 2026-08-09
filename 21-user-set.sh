df2f0a75388e999a415c229a0a5f9477180ba3d4
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-09 20:10:10 +0530

    RoboShop Project

---
+++ 21-user-set.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+
+failure(){
+    echo "failed at: $1 $2"
+}
+
+trap 'failure "${LINENO} ${BASH_COMMAND}"' ERR
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "You are running with the root access" | tee -a $LOG_FILE
+fi
+
+dnf module disable nodejs -y &>>$LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+dnf install nodejs -y &>>$LOG_FILE
+
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne  0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+else
+    echo -e "System user roboshop is already created ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app &>>$LOG_FILE
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+
+
+rm -rf /app/* &>>$LOG_FILE
+cd /app 
+unzip /tmp/user.zip &>>$LOG_FILE
+
+
+npm install &>>$LOG_FILE
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable user &>>$LOG_FILE
+systemctl start user &>>$LOG_FILE
+
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
