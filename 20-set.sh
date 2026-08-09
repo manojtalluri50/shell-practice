62cae00e314a0b2eb66ab8d5f27fe996aaef0929
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-09 19:58:01 +0530

    RoboShop Project

--- 20-set.sh
+++ 20-set.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e
+
 echo "Hi, Good Morning"
 
 echooo "Hi, these is an error"
+
+# if you encounter an error in 5th line you will be continue executing the next command
 
 echo "Hi, Morning"
