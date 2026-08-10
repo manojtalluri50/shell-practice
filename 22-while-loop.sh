900628e7b046a4ff998907fe7101584db6ecd86e
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-10 13:27:08 +0530

    RoboShop Project

--- 22-while-loop.sh
+++ 22-while-loop.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-a=0
+# a=0
 
-while [ $a -lt 10 ] 
-do
-    echo $a
-    a=`expr $a + 1`
-done
+# while [ $a -lt 10 ] 
+# do
+#     echo $a
+#     a=`expr $a + 1`
+# done
+
+
+while IFS= read -r line
+do 
+
+done < 21-user-set.sh
