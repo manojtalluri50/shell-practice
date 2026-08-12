f292a7dbd6336a2fc24f99fabd9fd70e32e1f7a0
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-12 19:14:51 +0530

    RoboShop Project

---
+++ 24-backup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # i* DAYS provided that will be considered otherwise default 14 days
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+CHECK_ROOT
+
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 24-backup.sh <source-dir> <destination-dir> <days(optional)>"
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+
+
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "You are running with the root access" | tee -a $LOG_FILE
+    fi
+
+}
