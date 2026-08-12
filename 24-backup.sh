94f2ffbbcf07374144ca1cccc8585db892c4be41
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-12 21:08:34 +0530

    RoboShop Project

--- 24-backup.sh
+++ 24-backup.sh
@@ -66,7 +66,7 @@ then
     echo "Files to zip are: $FILES"
     TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    
+    echo $FILES | zip -@ $ZIP_FILE
 else
     echo -e "No log files found older than 14 days ... $Y SKIPPING $N"
 fi
