2153146f9f7cfd5af61d5e622024231b26623164
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-12 19:45:28 +0530

    RoboShop Project

--- 24-backup.sh
+++ 24-backup.sh
@@ -65,5 +65,5 @@ if [ ! -z $FILES ]
 then
     echo "Files found"
 else
-    echo "No files found older than 14 days ... $Y SKIPPING $N"
+    echo -e "No files found older than 14 days ... $Y SKIPPING $N"
 fi
