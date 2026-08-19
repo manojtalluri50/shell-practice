d1a5aaea027309a0b0d9027d03983c703b55b0a7
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-19 17:25:24 +0530

    RoboShop Project

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -6,6 +6,6 @@ DISK_THRESHOLD=1
 while IFS= read line
 do
     USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-    PARITION=$(echo $line | awk '{print $6F}')
+    PARITION=$(echo $line | awk '{print $7F}')
     echo "$PARITION: $USAGE"
 done <<< $DISK_USAGE
