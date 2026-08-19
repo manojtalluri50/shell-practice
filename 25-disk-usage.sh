12fb198d62d5ea76968094f54ec564909f1a9122
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-19 17:24:32 +0530

    RoboShop Project

---
+++ 25-disk-usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem) 
+DISK_THRESHOLD=1
+
+while IFS= read line
+do
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARITION=$(echo $line | awk '{print $6F}')
+    echo "$PARITION: $USAGE"
+done <<< $DISK_USAGE
