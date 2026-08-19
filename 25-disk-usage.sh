96cf95349f5b6c195d75d8e0816a2e32cb7dbb7a
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-19 17:32:53 +0530

    RoboShop Project

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -2,10 +2,16 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem) 
 DISK_THRESHOLD=1
+MSG=""
 
 while IFS= read line
 do
     USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-    PARITION=$(echo $line | awk '{print $7F}')
-    echo "$PARITION: $USAGE"
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -gt $DISK_THRESHOLD ]
+    then
+        MSG="High Disk Usage on $PARTITION: $USAGE"
+    fi
 done <<< $DISK_USAGE
+
+echo $MSG
